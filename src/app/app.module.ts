66f381081379bd8ce6699a506edb0baa3c06565d
varaprasadreddy9676 <varaprasadreddy9676@gmail.com>
2020-08-20 01:29:34 +0530

    side-nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarModule } from 'ng-sidebar'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
